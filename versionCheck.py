17f81221741919fbf94121f6050cb0aae610ff18
Panayioti Koukoulas <koukoula@cs.ualberta.ca>
2017-01-11 18:01:39 -0700

    Updated to finish lab

--- versionCheck.py
+++ versionCheck.py
@@ -8,3 +8,6 @@ response = requests.get("http://google.com/")
 
 print response.text
 print response.status_code
+
+response = requests.get("https://raw.githubusercontent.com/Koukoula/cmput404w17lab1web/master/versionCheck.py")
+print response.text
