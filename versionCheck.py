7ec2f58a41ca731448e427e6defa749c5533442d
Panayioti Koukoulas <koukoula@cs.ualberta.ca>
2017-01-11 18:04:05 -0700

    commented earlier code in lab

--- versionCheck.py
+++ versionCheck.py
@@ -2,12 +2,13 @@
 
 import requests
 
-print requests.__version__
+#print requests.__version__
 
-response = requests.get("http://google.com/")
+#response = requests.get("http://google.com/")
 
-print response.text
-print response.status_code
+#print response.text
+#print response.status_code
 
 response = requests.get("https://raw.githubusercontent.com/Koukoula/cmput404w17lab1web/master/versionCheck.py")
 print response.text
+print response.status_code
